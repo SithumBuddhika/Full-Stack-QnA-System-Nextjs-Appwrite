fabb8ac933de0c22bb81f9f03c58205fb7b857e2
Sithum Buddhika Jayalal <Officialsithumbuddhika@gmail.com>
2026-01-06 15:55:53 +0530

    Nextjs and Appwrite Setup and Data Modeling

---
+++ src/models/server/comment.collection.ts
@@ -0,0 +1,42 @@
+import { Permission } from "node-appwrite";
+import { db, commentCollection } from "../name";
+import { databases } from "./config";
+
+export default async function createCommentCollection() {
+  // create collection
+  await databases.createCollection(db, commentCollection, commentCollection, [
+    Permission.create("users"),
+    Permission.read("any"),
+    Permission.read("users"),
+    Permission.update("users"),
+    Permission.delete("users"),
+  ]);
+  console.log("Comment Collection Created");
+
+  // creating attributes
+  await Promise.all([
+    databases.createStringAttribute(
+      db,
+      commentCollection,
+      "content",
+      10000,
+      true
+    ),
+    databases.createEnumAttribute(
+      db,
+      commentCollection,
+      "type",
+      ["answer", "question"],
+      true
+    ),
+    databases.createStringAttribute(db, commentCollection, "typeId", 50, true),
+    databases.createStringAttribute(
+      db,
+      commentCollection,
+      "authorId",
+      50,
+      true
+    ),
+  ]);
+  console.log("comment Attribute Created");
+}
